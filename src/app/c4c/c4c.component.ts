c56374b0e3d45be706881b73ec16e861dab6695f
Leah Sherrell and Maria Del Castillo <maria@>
2017-07-10 16:46:32 -0700

    add post-detail component and service

--- src/app/c4c/c4c.component.ts
+++ src/app/c4c/c4c.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
 import { Router } from '@angular/router';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-c4c',
   templateUrl: './c4c.component.html',
-  styleUrls: ['./c4c.component.css']
+  styleUrls: ['./c4c.component.css'],
+  providers: [PostService]
 })
-export class C4cComponent {
-  constructor(private router: Router){}
-  posts: Post[] = [
-    new Post('Lonely Racoon Seeks Night-time Cavorting', 'I\'m down to dig in any kind of garbage, if you catch my drift.', 3, 1),
-    new Post('Play Possum', 'Vision-impaired w/f here to find my seeker.', 100, 2)
+export class C4cComponent implements OnInit {
+  posts: Post[];
+  constructor(private router: Router, private postService: PostService){}
 
-  ];
+  ngOnInit(){
+    this.posts = this.postService.getPosts();
+    }
 
   goToDetailPage(clickedPost: Post) {
      this.router.navigate(['posts', clickedPost.id]);
---
+++ src/app/post.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Post } from './post.model';
+import { POSTS } from './mock-posts';
+
+@Injectable()
+export class PostService {
+
+  constructor() { }
+
+  getPosts() {
+    return POSTS;
+  }
+
+}
